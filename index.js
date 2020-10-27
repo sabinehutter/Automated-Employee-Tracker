4c5c41e55f1a81883c6035e4cabbf895c5a59032
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-26 23:00:19 -0500

    adding in switches

--- index.js
+++ index.js
@@ -24,9 +24,73 @@ async function IntroPrompts(){
         name: "choice", 
         message: "What would you like to do?", 
         choices: [
-            
+            {
+                name: "View Employees",
+                value: "EMPLOYEES"
+            },
+            {
+                name : "View Employees By Department",
+                value : "EMPLOYEES_BY_DEPT"
+            },
+            {
+                name : "View Employess By Manger",
+                value : "EMPLOYEES_BY_MANGAEER"
+            },
+            {
+                name : "Add New Employeee", 
+                value : "NEW_EMPLOYEE"
+            },
+            {
+                name : "Remove Employee",
+                value : "DELETE_EMPLOYEE"
+            },
+            {
+                name : "Update Employees' Role",
+                value : "UPDATE_ROLE"
+            },
+            {
+                name : "Update Employees' Manager",
+                value : "UPDATE_MANAGER"
+            },
+            {
+                name : "View All Roles",
+                value : "VIEW_ALL"
+            },
+            {
+                name : "Add Role",
+                value : "ADD_ROLE"
+            },
+            {
+                name : "Remove Role",
+                value : "REMOVE_ROLE"
+            },
+            {
+                name : "View Departments",
+                value : "VIEW_ALL_DEPT"
+            },
+            {
+                name : "Add Department",
+                value : "ADD_DEPT"
+            },
+            {
+                name : "Remove Department",
+                value : "DELETE_DEPT"
+            },
+            {
+                name : "Quit",
+                value : "QUIT"
+            }
         ]
-    }])
+    }
+]);
+
+switch(choice){
+    case "EMPLOYEES":
+        return viewEmployees();
+    case "EMPLOYEES_BY_DEPT":
+        return viewEmployeesByDept();
+}
+
 }
 
     // ask 
