28bb81de4d36d29655d24344860ce99957496f65
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-26 22:58:25 -0500

    adding in quetions

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const connection = require("./connection");
+
+class DB{
+    constructor(connection){
+        this.connection = connection;
+    }
+
+    findAllEmployees(){
+        return this.connection.query(
+           "SELECT * FROM EMPLOYEE"
+            // "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFTT JOIN department on role.department_id"
+        )
+    }
+
+}
+
+module.exports = new DB(connection)
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 // require inquirer, mysql
+const {prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init(){
+    const introText = logo({ name : "Employee Manager"}).render()
+
+    console.log(introText)
+
+    IntroPrompts();
+}
 
 // make a connection 
 
 
 // main menu 
+async function IntroPrompts(){
+    const { choice } = await([{
+        type: "list", 
+        name: "choice", 
+        message: "What would you like to do?", 
+        choices: [
+            
+        ]
+    }])
+}
 
     // ask 
         // Add (departments, roles, employees)
