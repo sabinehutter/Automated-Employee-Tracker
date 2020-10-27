3b16acb940ecb8145a211b34722f3d4986375de5
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-26 23:42:04 -0500

    fixing connection bug

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user : "root",
     password : "password123",
-    database : "employees"
+    database : "employee"
 });
 
 connection.connect();
