7b4900d64a90068e8d7967699fb50ffed2116a68
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-26 22:30:27 -0500

    subo coding

---
+++ index.js
@@ -0,0 +1,28 @@
+// require inquirer, mysql
+
+// make a connection 
+
+
+// main menu 
+
+    // ask 
+        // Add (departments, roles, employees)
+
+        //View (departments, roles, employees)
+
+        // Update (employee roles)
+
+// 3 functions
+// function add()
+    // ask what table you want to add to
+    // make query (connection.query) - insert into 
+
+// function view()
+    // ask whatt table you want to view
+    // make query (connection.query) - select
+
+
+// function update()
+    // ask what table you want to update
+    // make query (connection.query) - updae ... set ..
+
